615e27b17cf265cc4c7f5e041eeceb0684e499b7
Sean Reece <reece.sean@gmail.com>
2015-01-20 20:18:54 -0500

    Improve error handling

--- routes/users.js
+++ routes/users.js
@@ -4,21 +4,23 @@ var User = mongoose.model('Users');
 exports.register = function(req, res, next){
     var user = new User();
     user.save(function(err, doc) {
-        if (err) return console.error(err);
+        if (err){
+            next(err);
+        }
         console.dir(doc);
         res.send({apiKey: doc._id});
     });
 };
 
 exports.check = function(req,res,next){
-    if(!req.query.apiKey){
+    if(!req.query.apiKey || req.query.apiKey == 'undefined'){
         res.status(401).end();  //No authentication provided
     }
     else{
         User.findById(req.query.apiKey,function(err, user){
             if(err){
                 console.log("Error authenticating user "+req.query.apiKey+" - "+err.message);
-                res.status(500).end();
+                next(err);
             }
             else if(user){
                 req.user = user;
